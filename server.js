f82120a4f39729b18c33e36c9a61050c4974b7ea
Ivan Salinas <isalinas.marquez@outlook.com>
2016-10-25 08:47:17 -0500

    Se instalaron los modulos, express, pug, gulp, gulp-sass, materialize

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res){
+	res.render('index')
+})
+
+app.listen(3000, function(err) {
+	if(err) {
+		return console.log("Hubo un error"), process.exit(1)
+	} else {
+		console.log("Escuchando en el puerto 3000");
+	}
+})
